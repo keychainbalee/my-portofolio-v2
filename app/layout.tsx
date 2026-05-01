61107e6396601d565504387b1f886e51d295ae26
muhammadiqbalsaputra <muhammad.iqbal.sap@gmail.com>
2026-05-01 22:50:25 +0700

    update : tittle

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: "My Portofolio",
+  title: "Iqbal's | Portofolio",
   description: "Deskripsi project yang menarik",
 }
 
